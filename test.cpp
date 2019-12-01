4c3391cdd0ca0b0c106b969e02bf56227961413b
Alex Master <alex.macromaster@outlook.com>
2019-12-01 12:47:39 +0200

    do it in C

--- test.cpp
+++ test.cpp
@@ -1,23 +1,31 @@
+#include <algorithm>
 #include <iostream>
 #include <vector>
 
 #define INF 2147483647
 using std::string, std::vector, std::pair, std::cout, std::endl;
 
-// void print_way(vector<vector<int>> &children, vector<int> way, int length) {
-//     if (length) {
-        
-//         // way.push_back();
-//         print_way(children, way, length - 1);
-//     }
-// }
+void print_way(vector<vector<int>> &parents, vector<int> &way, int start) {
+    for (int i = 0; i < parents[way.back()].size(); ++i) {
+        way.push_back(parents[way.back()][i]);
+
+        if (start != way.back())
+            print_way(parents, way, start);
+        else {
+            cout << "Route: ";
+            for (const auto &i : way)
+                cout << i << ", ";
+            cout << endl;
+        }
+        way.pop_back();
+    }
+}
 
 void dijkstra(vector<vector<pair<int, int>>> &graph, int start) {
     vector<int> distances(graph.size(), INF);
     distances[start] = 0;
 
     vector<vector<int>> parents(graph.size());
-    vector<vector<int>> children(graph.size());
     vector<char> visited(graph.size(), false);
 
     for (size_t i = 0, v = INF, _size = graph.size(); i < _size; ++i, v = INF) {
@@ -40,10 +48,6 @@ void dijkstra(vector<vector<pair<int, int>>> &graph, int start) {
         }
     }
 
-    for (int i = 0; i < parents.size(); ++i)
-        for (int j = 0; j < parents[i].size(); ++j)
-            children[parents[i][j]].push_back(i);
-
     cout << string(40, '=') << endl;
 
     int begin = start + 1;
@@ -53,7 +57,9 @@ void dijkstra(vector<vector<pair<int, int>>> &graph, int start) {
 
     cout << string(40, '-') << endl;
 
-    for (size_t i = begin, end = parents.size(); i < end; ++i) {
+    for (size_t i = 0, end = parents.size(); i < end; ++i) {
+        std::sort(parents[i].begin(), parents[i].end());
+
         cout << "P: " << i << " : ";
         for (const auto &j : parents[i])
             cout << j << ", ";
@@ -62,24 +68,22 @@ void dijkstra(vector<vector<pair<int, int>>> &graph, int start) {
 
     cout << string(40, '-') << endl;
 
-    for (size_t i = 0, end = children.size(); i < end; ++i) {
-        cout << "C: " << i << " : ";
-        for (const auto &j : children[i])
-            cout << j << ", ";
-        cout << endl;
+    vector<int> way;
+
+    for (int i = 1, end = parents.size(); i < end; ++i) {
+        way.push_back(i);
+        print_way(parents, way, start);
+        way.pop_back();
     }
-
-    // cout << string(40, '-') << endl;
-
-    // for (int i = 0; i < graph.size(); ++i)
-    //     print_way(children, vector<int>(), i);
 
     cout << string(40, '=') << endl;
 }
 
 int main() {
-    size_t size = 5;
-    vector<vector<pair<int, int>>> graph(size, vector<pair<int, int>>());   // список вершин: спискок ребер: вершина | вес ребра
+    size_t size = 7;
+
+    // список вершин: спискок ребер: вершина | вес ребра
+    vector<vector<pair<int, int>>> graph(size, vector<pair<int, int>>());
 
     // ======================================== //
 
@@ -120,22 +124,22 @@ int main() {
     // D 3
     // E 4
 
-    graph[0].push_back({1, 11});
-    graph[0].push_back({2, 10});
+    // graph[0].push_back({1, 11});
+    // graph[0].push_back({2, 10});
 
-    graph[1].push_back({0, 11});
-    graph[1].push_back({3,  5});
+    // graph[1].push_back({0, 11});
+    // graph[1].push_back({3,  5});
 
-    graph[2].push_back({0, 10});
-    graph[2].push_back({3,  6});
-    graph[2].push_back({4, 15});
+    // graph[2].push_back({0, 10});
+    // graph[2].push_back({3,  6});
+    // graph[2].push_back({4, 15});
 
-    graph[3].push_back({1,  5});
-    graph[3].push_back({2,  6});
-    graph[3].push_back({4,  4});
+    // graph[3].push_back({1,  5});
+    // graph[3].push_back({2,  6});
+    // graph[3].push_back({4,  4});
 
-    graph[4].push_back({2, 15});
-    graph[4].push_back({3,  4});
+    // graph[4].push_back({2, 15});
+    // graph[4].push_back({3,  4});
 
     // ======================================== //
 
@@ -243,6 +247,53 @@ int main() {
     // graph[12].push_back({11, 1});
 
     // ======================================== //
+
+    // Home-BusStop,4
+    // BusStop-Work,7
+    // Home-Taxi,1
+    // Metro-Home,6
+    // Taxi-Work,10
+    // Work-Gym,2
+    // Metro-Work,8
+    // Taxi-Gym,12
+    // BusStop-Park,18
+    // Park-Metro,16
+
+    // 0 Home
+    // 1 BusStop
+    // 2 Work
+    // 3 Taxi
+    // 4 Metro
+    // 5 Gym
+    // 6 Park
+
+    graph[0].push_back({1, 4});
+    graph[0].push_back({3, 1});
+    graph[0].push_back({4, 6});
+
+    graph[1].push_back({0, 4});
+    graph[1].push_back({2, 7});
+    graph[1].push_back({6, 18});
+
+    graph[2].push_back({1, 7});
+    graph[2].push_back({3, 10});
+    graph[2].push_back({4, 8});
+    graph[2].push_back({5, 2});
+
+    graph[3].push_back({0, 1});
+    graph[3].push_back({2, 10});
+    graph[3].push_back({5, 12});
+
+    graph[4].push_back({0, 6});
+    graph[4].push_back({2, 8});
+    graph[4].push_back({6, 16});
+
+    graph[5].push_back({2, 2});
+    graph[5].push_back({3, 12});
+
+    graph[6].push_back({1, 18});
+    graph[6].push_back({5, 16});
+
 
     for (int i = 0; i < size - 1; ++i)
         dijkstra(graph, i);
