fc33e08f5130f5bf77591ba8079b07c89127017f
Alex Master <alex.macromaster@outlook.com>
2020-01-14 12:43:59 +0200

    errors

--- Pathfinder/libmx/src/file/mx_get_line.c
+++ Pathfinder/libmx/src/file/mx_get_line.c
@@ -26,18 +26,18 @@ long long mx_get_line(char **lineptr, char delim, const int fd) {
     return *lineptr ? mx_strlen(*lineptr) : -1;
 }
 
-#include <stdio.h>
+// #include <stdio.h>
 
-int main() {
-    char *str;
-    int stream = open("txt.txt", 0);
+// int main() {
+//     char *str;
+//     int stream = open("txt.txt", 0);
 
-    printf(":%lli:%zu:%s:\n", mx_read_line(&str, '1', stream), malloc_size(str), str);
-    printf(":%lli:%zu:%s:\n", mx_read_line(&str, '\n', stream), malloc_size(str), str);
-    printf(":%lli:%zu:%s:\n", mx_read_line(&str, '\0', stream), malloc_size(str), str);
+//     printf(":%lli:%zu:%s:\n", mx_read_line(&str, '1', stream), malloc_size(str), str);
+//     printf(":%lli:%zu:%s:\n", mx_read_line(&str, '\n', stream), malloc_size(str), str);
+//     printf(":%lli:%zu:%s:\n", mx_read_line(&str, '\0', stream), malloc_size(str), str);
 
-    close(stream);
-    free(str);
+//     close(stream);
+//     free(str);
 
-    system("leaks -q a.out");
-}
+//     system("leaks -q a.out");
+// }
